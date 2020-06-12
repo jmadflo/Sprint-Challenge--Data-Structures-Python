efe9265c7ef846c111859f0cc0bd8a24984578c5
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-06-12 08:22:39 -0700

    ring buffer class operational

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.content = []
+        self.oldest = -1
 
     def append(self, item):
-        pass
-
+        if len(self.content) < self.capacity:
+            self.content.append(item)
+        else:
+            if self.oldest >= self.capacity - 1:
+                self.oldest = 0
+            else:
+                self.oldest += 1
+            self.content[self.oldest] = item
     def get(self):
-        pass
+        return self.content
