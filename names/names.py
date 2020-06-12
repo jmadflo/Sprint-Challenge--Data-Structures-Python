98adb7d98bf7e695c0c1efe6bb841fcb35d4263d
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-06-12 09:54:31 -0700

    cut execution time of names.py by over 80%

--- names/names.py
+++ names/names.py
@@ -13,10 +13,16 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+# my way took 1.9623067378997803 seconds to execute
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
+
+# default way took 10.374504089355469 seconds to execute
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
